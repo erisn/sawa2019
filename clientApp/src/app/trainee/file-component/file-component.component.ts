c9d0736820cc11476cd8e412f586b1cd10332729
Yebio Mesfin <ymesfin8@gmail.com>
2019-04-06 16:42:38 -0400

    file upload

--- clientApp/src/app/trainee/file-component/file-component.component.ts
+++ clientApp/src/app/trainee/file-component/file-component.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-file-component',
   templateUrl: './file-component.component.html',
   styleUrls: ['./file-component.component.css']
 })
-export class FileComponentComponent implements OnInit {
+export class FileComponentComponent implements OnInit, OnChanges {
+  file: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  upload(){
+    console.log('I am clicked .....');
+  }
+
+  upload2(event){
+    console.log('I am clicked event trigger');
+  }
+
+  ngOnChanges() {
+    console.log('I am clicked event trigger *****');
+  }
 }
